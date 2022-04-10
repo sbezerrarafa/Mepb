48bd6df1e80770696ba86cd9963f4a78f271d396
rbezerrajc <rbezerra@sjcc.com.br>
2022-04-10 16:52:04 -0300

    ajuste

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('home', function () {
+    return view('home.home');
+});
